a98028bdf03aa62dceaf32c3c89d5495af89fc04
Or Brener <obrener@uoguelph.ca>
2023-03-31 14:07:16 -0400

    add files

---
+++ textyzer.adb
@@ -0,0 +1,13 @@
+--  Name: Or Brener
+--  Student Id: 1140102
+--  CIS*3190 A4 W23
+
+with Ada.Text_IO; use Ada.Text_IO;
+-- with ada.Integer_Text_IO; use Ada.Integer_Text_IO;
+
+procedure textyzer is    
+    begin
+
+    Put_Line ("Hello World!");
+
+end textyzer;
